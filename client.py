3990d2dbe4966f9c8f3fe22d8d9f91f59a9bd9df
ehamayel <eman@ubuntu.asaltech.com>
2018-10-23 05:44:34 -0400

    feat: add get port number, and get os information functions

--- client.py
+++ client.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
 import rpyc
 c = rpyc.connect("localhost", 12345)
-print c.root.get_ip()
+
+print "The ip", c.root.get_ip()
+print 'listening on port:', c.root.get_port()
+os_info = c.root.get_os_information()
+print "System name", os_info[0]
+print "hostname", os_info[1]
+print "release", os_info[2]
+print "version", os_info[3]
+print "machine type", os_info[4]
--- server.py
+++ server.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 import rpyc
+import socket
+import os
+import platform
+import multiprocessing
 class MyService(rpyc.Service):
     def on_connect(self, conn):
         print "A client connected to the server" 
         pass
+
     def on_disconnect(self, conn):
         print "A client diss connected from the server"
         pass
+
     def exposed_get_ip(self):
-        import socket
-        ip=socket.gethostbyname(socket.gethostname())
-        print ip
+        ip = socket.gethostbyname(socket.gethostname())
         return ip
+
+    def exposed_get_port(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind(('0.0.0.0', 0))
+        port_num =  sock.getsockname()[1]
+        return port_num
+
+    def exposed_get_os_information(self):
+        #return platform.uname()
+        print os.uname()[0]
+        return os.uname() #object with five attributes:
+                          #sysname - operating system name
+                          #nodename - name of machine on network
+                          #release - operating system release
+                          #version - operating system version
+                          #machine - hardware identifier
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
