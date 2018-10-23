33231fea7a34a96224cc26a34de507463eae4293
ehamayel <eman@ubuntu.asaltech.com>
2018-10-23 05:49:54 -0400

    style: change print format, no code edited

--- client.py
+++ client.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 import rpyc
 c = rpyc.connect("localhost", 12345)
-
-print "The ip", c.root.get_ip()
-print 'listening on port:', c.root.get_port()
+print "{:<30} {:<30}".format('The IP:', c.root.get_ip())
+print "{:<30} {:<30}".format('Listening on port:', c.root.get_port())
 os_info = c.root.get_os_information()
-print "System name", os_info[0]
-print "hostname", os_info[1]
-print "release", os_info[2]
-print "version", os_info[3]
-print "machine type", os_info[4]
+print "{:<30} {:<30}".format('System name:', os_info[0])
+print "{:<30} {:<30}".format('Hostname:', os_info[1])
+print "{:<30} {:<30}".format('Release:',os_info[2])
+print "{:<30} {:<30}".format('Version:',os_info[3])
+print "{:<30} {:<30}".format('Machine type:', os_info[4])
+
