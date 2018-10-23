36346330d3ad865212e72c90bc44cd10bd2611a5
ehamayel <eman@ubuntu.asaltech.com>
2018-10-23 08:39:13 -0400

    feat: change attributes access policy to 'public'

--- server.py
+++ server.py
@@ -15,17 +15,17 @@ class MyService(rpyc.Service):
         print "A client diss connected from the server"
         pass
 
-    def exposed_get_ip(self):
+    def get_ip(self):
         ip = socket.gethostbyname(socket.gethostname())
         return ip
 
-    def exposed_get_port(self):
+    def get_port(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(('0.0.0.0', 0))
         port_num =  sock.getsockname()[1]
         return port_num
 
-    def exposed_get_os_information(self):
+    def get_os_information(self):
         #return platform.uname()
         return os.uname() #object with five attributes:
                           #sysname - operating system name
@@ -34,19 +34,20 @@ class MyService(rpyc.Service):
                           #version - operating system version
                           #machine - hardware identifier
 
-    def exposed_get_boot_time(self):
+    def get_boot_time(self):
         #return server uptime in 'YYYY-MM-DD HH:MM:SS'
         return datetime.datetime.fromtimestamp(
                psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
 
-    def exposed_get_proccess_info(self):
+    def get_proccess_info(self):
         return psutil.cpu_count()
 
-    def exposed_get_available_ram(self):
+    def get_available_ram(self):
         return psutil.virtual_memory().active
         
 
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(MyService, port=12345)
+    t = ThreadedServer(MyService, port=12345,
+                       protocol_config={'allow_public_attrs': True})
     t.start()
